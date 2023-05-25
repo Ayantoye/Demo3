13a62eafacd5e694709fad2e204903afbdc6ef54
Ayantoye <133983725+Ayantoye@users.noreply.github.com>
2023-05-25 10:22:42 -0400

    Update DemoGit.java
    news changes

--- src/demogit/DemoGit.java
+++ src/demogit/DemoGit.java
@@ -16,6 +16,9 @@ public class DemoGit {
      */
     public static void main(String[] args) {
         System.out.println("hello this is my first git operation");
+        int a;
+        int b;
+        int c= a+b;
     }
 
 }
